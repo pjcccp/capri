4d6bc2157a35351e1ac0f3af9b84b97a3aebc330
Paul Jolley <paul.e.jolley@gmail.com>
2012-03-29 20:19:33 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capri::Application.config.secret_token = '02e1918254142370d1b39ed1e705512b27d60afeaa8bb4028c931550dab76f56d2aa72cf94b07119439f1846bac4c6e0d5b4b3bb2255580ce38a8fe61527e834'
