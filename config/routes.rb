4ddfaabc8d72017ee900a6cf66b9a796c89f346f
Paul Jolley <paul.e.jolley@gmail.com>
2012-03-30 00:17:20 -1000

    admin update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Capri::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :tasks
 
   # The priority is based upon order of creation:
